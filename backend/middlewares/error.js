c34db3ced933747d1e8e5d5d4535bcfaa3a039d0
Aniket Sharma <tpbc1717@outlook.com>
2024-02-26 23:13:25 +0530

    Initial Commit

---
+++ backend/middlewares/error.js
@@ -0,0 +1,34 @@
+class ErrorHandler extends Error {
+    constructor(message, statusCode){
+        super (message);
+        this.statusCode = statusCode;
+    }
+}
+
+export const errorMiddleware = (err, req, res, next) =>{
+    err.message = err.message || "Interal Server Error";
+    err.statusCode = err.statusCode || 500;
+
+    if(err.name === "CaseError"){
+        const message = `Resource not found. Invalid ${err.path}`;
+        err = new ErrorHandler(message, 400);
+    }
+    if(err.name === 11000){
+        const message = `Dulicate ${Object.keys(err.keyValue)} Entered`;
+        err = new ErrorHandler(message, 400);
+    }
+    if(err.name === "JsonWebTokenError"){
+        const message = `Json web token error, Try Again`;
+        err = new ErrorHandler(message, 400);
+    }
+    if(err.name === "TokenExpiredError"){
+        const message = `JSON Web Token is expired, Try Again`;
+        err = new ErrorHandler(message, 400);
+    }
+    return res.status(err.statusCode).json({
+        success : false,
+        message : err.message,
+    });
+}
+
+export default ErrorHandler;
---
+++ backend/routes/userRouter.js
@@ -0,0 +1,10 @@
+import express from "express";
+import {register, login, logout, getUser} from "../controllers/userController.js"
+import { isAuthorized } from "../middlewares/auth.js";
+
+const router = express.Router();
+router.post("/register", register);
+router.post("/login", login);
+router.get("/logout",logout);
+router.get("/getuser",isAuthorized, getUser);
+export default router;
